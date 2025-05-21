ecd5590d8857e7dfba200a054bb79c4651d007df
ayush-verma-004 <ayushverma593773@gmail.com>
2025-05-21 21:59:16 +0530

    Add more file

---
+++ javnic/src/main/java/com/example/javnic/JavnicApplication.java
@@ -0,0 +1,13 @@
+package com.example.javnic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavnicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavnicApplication.class, args);
+	}
+
+}
---
+++ javnic/src/test/java/com/example/javnic/JavnicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javnic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavnicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
