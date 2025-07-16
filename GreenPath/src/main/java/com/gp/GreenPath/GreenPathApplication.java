5eaa2d02daf72f4b7fb73c87b7e3280c1ebba430
ayush-verma-004 <ayushverma593773@gmail.com>
2025-07-16 09:32:22 +0530

    Update my project

---
+++ GreenPath/src/main/java/com/gp/GreenPath/GreenPathApplication.java
@@ -0,0 +1,13 @@
+package com.gp.GreenPath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenPathApplication.class, args);
+	}
+
+}
